e373181c3c97843a789837463362888c1f4f6671
Gabriel Silva <gabiles278@gmail.com>
2024-07-03 01:07:34 -0300

    connection in database and create students and users tables

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,14 +2,16 @@ import  dotenv  from 'dotenv';
 dotenv.config();
 import './src/config/database';
 import express from 'express';
-import routes from './src/routes/home';
+import homeRoutes from './src/routes/home';
+import userRoutes from './src/routes/user';
 const app = express();
 
 //Config response for forms and others
 app.use(express.urlencoded({ extended: true }));
 //Config response for jsons
 app.use(express.json());
-app.use(routes);
+app.use(homeRoutes);
+app.use(userRoutes);
 
 
 app.listen(3000, () => {
